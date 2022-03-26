3d360b26d47d6bb29ab4f34ae2ca3756198c8434
Antoine LRK <contact@antoineleroy.fr>
2022-03-26 16:32:43 +0100

    HER-18 - Added message rewriting by the bot on MessageReceived listener

---
+++ src/main/java/fr/blueberry/studio/hermes/listeners/discord/MessageReceivedListener.java
@@ -0,0 +1,41 @@
+package fr.blueberry.studio.hermes.listeners.discord;
+
+import org.simpleyaml.configuration.file.YamlFile;
+import java.awt.Color;
+import fr.blueberry.studio.hermes.api.bots.BotManager;
+import fr.blueberry.studio.hermes.api.utils.ColorHelper;
+import fr.blueberry.studio.hermes.api.utils.MessageEmbedHelper;
+import fr.blueberry.studio.hermes.api.bots.Bot;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+public class MessageReceivedListener extends ListenerAdapter {
+    private final BotManager botManager;
+    private final YamlFile config;
+
+    public MessageReceivedListener(BotManager botManager, YamlFile config) {
+        this.botManager = botManager;
+        this.config = config;
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        final Bot bot = this.botManager.pickRandomBot(true);
+        final TextChannel eventTextChannel = event.getGuild().getTextChannelById(config.getLong("eventChannel"));
+        final Color color = ColorHelper.toRGB(config.getString("color"));
+
+        if (eventTextChannel != event.getTextChannel()) return;
+        if (event.getAuthor().isBot()) return;
+
+        final MessageEmbed embed = MessageEmbedHelper.getBuilder()
+            .setTitle("La blague de " + event.getMember().getUser().getName())
+            .setDescription(event.getMessage().getContentRaw())
+            .setColor(color)
+            .setThumbnail(event.getMember().getUser().getAvatarUrl())
+            .build();
+            
+        bot.sendMessageTo(eventTextChannel, embed);
+    }
+}
