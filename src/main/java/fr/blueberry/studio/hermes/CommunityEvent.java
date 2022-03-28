0b0975f471c20e0d60e3400cbd005030d6a87d16
Antoine LRK <contact@antoineleroy.fr>
2022-03-28 17:11:45 +0200

    HER-6 - Added victory message embed

--- src/main/java/fr/blueberry/studio/hermes/CommunityEvent.java
+++ src/main/java/fr/blueberry/studio/hermes/CommunityEvent.java
@@ -2,10 +2,13 @@ package fr.blueberry.studio.hermes;
 
 import fr.blueberry.studio.hermes.api.plugins.Plugin;
 import fr.blueberry.studio.hermes.listeners.discord.MessageReceivedListener;
-
 import org.simpleyaml.configuration.file.YamlFile;
-
+import java.awt.Color;
 import fr.blueberry.studio.hermes.api.bots.BotManager;
+import fr.blueberry.studio.hermes.api.utils.ColorHelper;
+import fr.blueberry.studio.hermes.api.utils.MessageEmbedHelper;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class CommunityEvent extends Plugin {
     public static CommunityEvent INSTANCE;
@@ -21,5 +24,20 @@ public class CommunityEvent extends Plugin {
         final YamlFile config = getConfiguration();
 
         botManager.getJDAListenerManager().registerJDAListener(new MessageReceivedListener(botManager, config));
+    }
+
+    public static MessageEmbed getVictoryMessage (Member winner) {
+        final Color victoryColor = ColorHelper.toRGB(INSTANCE.getConfiguration().getString("victoryColor"));
+        final String prize = INSTANCE.getConfiguration().getString("prize");
+        final String iconUrl = INSTANCE.getConfiguration().getString("iconUrl");
+
+        final MessageEmbed victoryMessage = MessageEmbedHelper
+            .getBuilder()
+            .setTitle("Victoire !")
+            .setColor(victoryColor)
+            .setThumbnail(iconUrl)
+            .setDescription(winner.getAsMention() + " a gagné ! Il obtient le lot: " + prize)
+            .build();
+        return victoryMessage;
     }
 }
