98a931e28227c283fcc300fc1cd8dd8fbe3ad7af
Antoine LRK <contact@antoineleroy.fr>
2022-03-29 18:59:37 +0200

    HER-6 - Craft Embed from file config

--- src/main/java/fr/blueberry/studio/hermes/CommunityEvent.java
+++ src/main/java/fr/blueberry/studio/hermes/CommunityEvent.java
@@ -3,12 +3,12 @@ package fr.blueberry.studio.hermes;
 import fr.blueberry.studio.hermes.api.plugins.Plugin;
 import fr.blueberry.studio.hermes.listeners.discord.MessageReceivedListener;
 import org.simpleyaml.configuration.file.YamlFile;
-import java.awt.Color;
 import fr.blueberry.studio.hermes.api.bots.BotManager;
-import fr.blueberry.studio.hermes.api.utils.ColorHelper;
 import fr.blueberry.studio.hermes.api.utils.MessageEmbedHelper;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommunityEvent extends Plugin {
     public static CommunityEvent INSTANCE;
@@ -27,17 +27,12 @@ public class CommunityEvent extends Plugin {
     }
 
     public static MessageEmbed getVictoryMessage (Member winner) {
-        final Color victoryColor = ColorHelper.toRGB(INSTANCE.getConfiguration().getString("victoryColor"));
-        final String prize = INSTANCE.getConfiguration().getString("prize");
-        final String iconUrl = INSTANCE.getConfiguration().getString("iconUrl");
+        final Map<String, String> replacements =  new HashMap<String, String>();
 
-        final MessageEmbed victoryMessage = MessageEmbedHelper
-            .getBuilder()
-            .setTitle("Victoire !")
-            .setColor(victoryColor)
-            .setThumbnail(iconUrl)
-            .setDescription(winner.getAsMention() + " a gagné ! Il obtient le lot: " + prize)
-            .build();
-        return victoryMessage;
+        replacements.put("%player%", winner.getAsMention());
+        replacements.put("%prize%", INSTANCE.getConfiguration().getString("prize"));
+
+        MessageEmbed victoryMessageEmbed = MessageEmbedHelper.craftEmbedFromConfig("victoryMessage", INSTANCE.getConfiguration(), replacements);
+        return victoryMessageEmbed;
     }
 }
